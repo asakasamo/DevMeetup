c7ea41f1ca3caee62dc24b82b627b6ce81c760bf
asakasamo <a.sakasamo@gmail.com>
2018-07-30 05:18:03 -0500

    Initial commit: Setup main navigation toolbar and responsive nav drawer

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+   <v-app>
+      <!-- Navigation Drawer -->
+      <v-navigation-drawer v-model="sideNav" absolute disable-resize-watcher>
+         <v-list>
+            <v-list-tile v-for="item in menuItems" :key="item.title" @click="() => null">
+               <v-list-tile-action>
+                  <v-icon>{{ item.icon }}</v-icon>
+               </v-list-tile-action>
+               <v-list-tile-content>
+                  {{ item.title }}
+               </v-list-tile-content>
+            </v-list-tile>
+         </v-list>
+      </v-navigation-drawer>
+
+      <!-- Main Navigation Toolbar -->
+      <v-toolbar dark class="primary">
+         <v-toolbar-side-icon 
+            @click.native.stop="sideNav = !sideNav"
+            class="hidden-sm-and-up"
+            >
+         </v-toolbar-side-icon> 
+
+         <v-toolbar-title>Meetup</v-toolbar-title>
+         
+         <v-spacer></v-spacer>
+
+         <v-toolbar-items class="hidden-xs-only">
+            <v-btn flat v-for="item in menuItems" :key="item.title">
+               <v-icon left>{{ item.icon }}</v-icon>
+               {{ item.title }}
+            </v-btn>
+         </v-toolbar-items>
+      </v-toolbar>
+
+      <!-- Main Content Route -->
+      <router-view></router-view>
+   </v-app>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         sideNav: false,
+         menuItems: [
+            { icon: "supervisor_account", title: "View Meetups" },
+            { icon: "room", title: "Organize Meetup" },
+            { icon: "person", title: "Profile" },
+            { icon: "face", title: "Sign up" },
+            { icon: "lock_open", title: "Log in" }
+         ]
+      };
+   },
+   mounted() {}
+};
+</script>
+
+<style>
+</style>
