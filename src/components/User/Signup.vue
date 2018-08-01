4f7dda4310a8909c4d9c9e033abdd4d31c792470
asakasamo <a.sakasamo@gmail.com>
2018-08-01 02:08:46 -0500

    Scaffold the Signup page

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,5 +1,92 @@
 <template>
-   <div>
-      <h1>Signup Page</h1>
-   </div>
+   <v-container>
+      <v-layout>
+         <v-flex xs12 sm6 offset-sm3>
+
+            <!-- Signup card wrapper -->
+            <v-card>
+               <v-card-text>
+                  <form>
+                     <v-layout row>
+                        <v-flex xs12>
+
+                           <!-- Email field -->
+                           <v-text-field 
+                              name="email" 
+                              label="Email" 
+                              id="email" 
+                              v-model="email" 
+                              type="email" 
+                              required
+                              >
+                           </v-text-field>
+
+                           <!-- Password field -->
+                           <v-text-field 
+                              name="password" 
+                              label="Password" 
+                              id="password" 
+                              v-model="password" 
+                              type="password" 
+                              required
+                              >
+                           </v-text-field>
+
+                           <!-- Confirm password field -->
+                           <v-text-field 
+                              name="confirmPassword" 
+                              label="Confirm Password" 
+                              id="confirmPassword" 
+                              v-model="confirmPassword" 
+                              type="password"
+                              required
+                              :rules="[ comparePasswords ]"
+                              >
+                           </v-text-field>
+                        </v-flex>
+                     </v-layout>
+
+                     <!-- Submit button -->
+                     <v-layout row>
+                        <v-flex xs12>
+                           <v-btn type="submit">
+                              Sign up
+                           </v-btn>
+                        </v-flex>
+                     </v-layout>
+                  </form>
+               </v-card-text>
+            </v-card>
+         </v-flex>
+      </v-layout>
+   </v-container>
 </template>
+
+<script>
+export default {
+   data() {
+      return {
+         email: "",
+         password: "",
+         confirmPassword: ""
+      };
+   },
+   computed: {
+      comparePasswords() {
+         return this.password === this.confirmPassword
+            ? true
+            : "Passwords do not match";
+      }
+   },
+   methods: {
+      submitSignup() {
+         // TODO: Vuex store it in the database
+         console.log({
+            email: this.email,
+            password: this.password,
+            confirmPassword: this.confirmPassword
+         });
+      }
+   }
+};
+</script>
